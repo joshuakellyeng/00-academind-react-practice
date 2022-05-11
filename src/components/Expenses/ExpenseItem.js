d6b0fd759f93daa651a4c10ff761b203db869a9e
Joshua Kelly <joshuakellyeng@gmail.com>
2022-05-11 08:10:47 -0400

    added expense item component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
 import './ExpenseItem.css'
 
 
+
 const ExpenseItem = (props) => {
+
+    //function clickHandler(){}
+    const [title, setTitle] = useState(props.item.title)
+
+    const clickHandler = () =>{
+        setTitle('Updated')
+        console.log(title)
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.item.date} />
             <div className='expense-item__description'>
-                <h2>{props.item.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.item.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
